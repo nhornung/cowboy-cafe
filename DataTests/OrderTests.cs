6b550707e14b72bfb50dad22ef9ab4f9b6dc66ba
nick hornung <nphornung77@gmail.com>
2020-02-26 16:54:43 -0600

    First test cases written

---
+++ DataTests/OrderTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CowboyCafe.Data;
+
+namespace CowboyCafe.DataTests
+{
+    public class OrderTests
+    {
+        public class MockOrderItem : IOrderItem
+        {
+            public double Price { get; set; }
+            public List<string> SpecialInstructions { get; set; }
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToAddItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToRemoveItems()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+
+
+        public void ShouldBeAbleToGetAnEnumerationOfItems()
+        {
+            var order = new Order();
+            var item0 = new MockOrderItem();
+            var item1 = new MockOrderItem();
+            var item2 = new MockOrderItem();
+            order.Add(item0);
+            order.Add(item1);
+            order.Add(item2);
+            Assert.Collection(order.Items,
+                item => Assert.Equal(item0, item), // actual, expected
+                item => Assert.Equal(item1, item),
+                item => Assert.Equal(item2, item));
+        }
+
+
+        [Theory]
+        [InlineData(new double[] { 1, 2, 3 })]
+        [InlineData(new double[] { 0, 0, 0 })]
+        [InlineData(new double[] { 199.34, 799 })]
+        [InlineData(new double[] { 798 })]
+        [InlineData(new double[] { })]
+        [InlineData(new double[] { -5 })]
+        [InlineData(new double[] { -4, 10, 8 })]
+        [InlineData(new double[] { 3.14159265359 })]
+        [InlineData(new double[] { double.NaN })]
+        public void SubtotalShouldBeTheSumOfItemPrices(double[] prices)
+        {
+            var order = new Order();
+            double total = 0;
+            foreach(var price in prices)
+            {
+                total += price;
+                order.Add(new MockOrderItem()
+                {
+                    Price = price
+                });
+            }
+            Assert.Equal(total, order.SubTotal);
+        }
+
+
+        [Theory]
+        [InlineData("Price")]
+        [InlineData("Items")]
+        // ? should trigger PropertyChanged for Price
+        public void AddingAnItemShouldTriggerPropertyChanged(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Add(item);
+            });
+
+        }
+
+
+        [Theory]
+        [InlineData("Price")]
+        [InlineData("Items")]
+        public void RemovingAnItemShouldTriggerPropertyChangedForPrice(string propertyName)
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.PropertyChanged(order, propertyName, () =>
+            {
+                order.Remove(item);
+            });
+        }
+    }
+}
