f30888ac45042199010415e0d89ecdc457f23dbd
nick hornung <nphornung77@gmail.com>
2020-04-19 13:32:17 -0500

    struggling

---
+++ Data/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public static class Menu
+    {
+        private static List<IOrderItem> entreeList = new List<IOrderItem>();
+
+        private static List<IOrderItem> sideList = new List<IOrderItem>();
+
+        private static List<IOrderItem> drinkList = new List<IOrderItem>();
+
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            throw new NotImplementedException();
+            
+        }
+
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            throw new NotImplementedException();
+          
+        }
+
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            throw new NotImplementedException();
+           
+        }
+
+        public static IEnumerable<IOrderItem> CompleteMenu()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
